e4f62638b8ef8293e14ed868123f38ca6e86c2c3
Akash Kumar Mall <84437841+akashmall80@users.noreply.github.com>
2023-04-24 22:46:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const port = 8000;
+
+function requestHandler(req,res)
+{
+    // we will get the url
+    // for localhost:8000 it will give /
+    // for localhost:8000/user it will give /user
+    // for localhost:8000/prifle it will give /profile
+    console.log(req.url);
+
+    res.writeHead(200, {'content-type':'text/html'})
+    
+
+    //it will give response
+    res.end('<h1>Gothcha</h1>');
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, function (err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log("server is running", port);
+})
